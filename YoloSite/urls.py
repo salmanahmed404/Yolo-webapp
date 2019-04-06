199ef0d6eb07500262f53e245eb5c2f35ae25ee2
Salman <salmanahmed404@gmail.com>
2019-04-06 21:09:45 +0530

    Foreign key fixed

--- YoloSite/urls.py
+++ YoloSite/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from Yolo import views as Yolo_views
 from users import views as users_views
@@ -22,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',Yolo_views.home,name='home'),
     path('register',users_views.register,name='register'),
-    #path('login',views.login,name='login'),
-       
+    path('login',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout',auth_views.LogoutView.as_view(template_name='Yolo/home.html'),name='logout'),      
 ]
