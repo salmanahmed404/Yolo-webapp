fb2fd464b15fb06256b6e372b08069de605d7932
Salman <salmanahmed404@gmail.com>
2019-04-12 20:39:25 +0530

    Start hotels.html frontend

--- bookings/views.py
+++ bookings/views.py
@@ -3,11 +3,4 @@ import json
 from Yolo.models import City
 
 def hotels(request):
-    json_data = open('static/city_mock.json')
-    print(type(json_data))
-    data = json.load(json_data)
-    print(type(data))
-    for val in data:
-        name = City(city_name=val['city_name'])
-        name.save()
-    return render (request,'bookings/hotels.html')
+    return render (request,'bookings/hotels.html') 
