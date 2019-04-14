874136b0ddc33f93865428322f4e08affd430ecf
Tanzilur Rahman <rahman.tanzilur@outlook.com>
2019-04-14 20:21:52 +0530

    Corrected City UTF-8 error

--- YoloSite/settings.py
+++ YoloSite/settings.py
@@ -81,12 +81,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'yolo_database',
-        'USER': 'root',
-<<<<<<< HEAD
-        'PASSWORD': 'lari1234',
-=======
-        'PASSWORD': 'Shaima@123',
->>>>>>> 8ac33b7023a3d3a78aa73fc4ed1b8fdc2658f1bb
+        'USER': 'django',
+#<<<<<<< HEAD
+        'PASSWORD': 'root',
+#=======        'PASSWORD': 'Shaima@123',
+#>>>>>>> 8ac33b7023a3d3a78aa73fc4ed1b8fdc2658f1bb
         'HOST': 'localhost',
         'PORT': 3306
     }
