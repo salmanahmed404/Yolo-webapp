22be9cfb302de992e08ccc218543a673ab7f8975
Tanzilur Rahman <rahman.tanzilur@outlook.com>
2019-04-15 01:54:00 +0530

    Added debug toolbar

--- Yolo/views.py
+++ Yolo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login
+from django.db import connection
 
 def home(request):
     return render(request,'Yolo/home_new.html')
+
+def sql_queries(request):
+    print (connection.queries)
+    return render(request,'Yolo/sql_queries.html')
 
--- YoloSite/urls.py
+++ YoloSite/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 from Yolo import views as Yolo_views
 from users import views as users_views
 from bookings import views as bookings_views
+from django.conf import settings
+import debug_toolbar
 
 urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
     path('',Yolo_views.home,name='home'),
     path('register/',users_views.register,name='register'),
@@ -31,4 +34,5 @@ urlpatterns = [
     path('find_hotels/',bookings_views.find_hotels,name='find_hotels'),
     path('book/<int:pk>',bookings_views.book,name='book'),
     path('response/',bookings_views.response,name='response'),
+    path('sql_queries/',Yolo_views.sql_queries,name='sql_queries'),
 ]
