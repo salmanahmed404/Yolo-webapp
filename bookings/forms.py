7748c0e7916bc916bf7c1eeb27a956917332cfe2
shaima40 <shaimaaziz99@gmail.com>
2019-04-15 12:23:48 +0530

    shaima not yet committed

--- bookings/forms.py
+++ bookings/forms.py
@@ -6,10 +6,18 @@ class Select_City(forms.ModelForm):
         model = Hotels
         fields = ['city_name']
         labels : {
-            'city_name':'City',
+            'city_name':'City Name',
         }
 
 class Book_Hotel(forms.ModelForm):
     class Meta:
         model = Hotel_Input
         fields = ['name','room_type','number_of_rooms','number_of_adults','number_of_children','contact']
+        labels : {
+            'name':'Name',
+            'room_type' : 'Room Type',
+            'number_of_rooms' : 'Number of Rooms',
+            'number_of_adults' : 'Number of Adults',
+            'number_of_children' : 'Number of Children',
+            'contact' : 'Contact Number'
+        }
